61c8ed96509e1802315ac85a30db05d3bfc998a6
Robert J. Bischoff <bischrob@gmail.com>
2018-01-24 21:45:30 -0700

    This is the initial commit for the rRJB package created to hold personal functions.

---
+++ R/LoadandInstallPackages.R
@@ -0,0 +1,21 @@
+#' A function to install packages if they do not exist.
+#'
+#' This function checks if needed packages are installed and installs and loads all requested packages.
+#' @param my.packages List all packages needed. There is no default.
+#' @keywords library, install.packages
+#' @export
+#' @examples
+#' my.packages <- c('dplyr','ggplot2')
+#' myLibrary(my.packages)
+
+myLibrary <- function(my.packages){
+  usePackage <- function(p) 
+  {
+    if (!is.element(p, installed.packages()[,1]))
+      install.packages(p, dep = TRUE)
+    require(p, character.only = TRUE)
+  }
+  
+  lapply(my.packages, usePackage)
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+---
+title: "rRJB is a package to hold personal functions."
+output: html_document
+---
+
+## Purpose
+
+I have written this package as a place to add useful and commonly
+used functions that I can access from anywhere on github.
+
+## Install
+to install this package use this code:
+
+library(devtools)
+install_github("bischrob/rRJB", auth_token = cb3c5d6aa39b210d0287470e7a135a4542d98d04)
