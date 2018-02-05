01d0d2b1903018c6dff4e8e82bf87dca45ee8efa
Robert Bischoff <bischrob@byu.local>
2018-02-05 15:18:10 -0700

    Added option to suppress warnings

--- R/myLibrary.R
+++ R/myLibrary.R
@@ -1,21 +1,28 @@
 #' A function to install packages if they do not exist
 #'
-#' This function checks if needed packages are installed and installs and loads all requested packages.
-#' @param my.packages List all packages needed. There is no default.
+#' This function checks if needed packages are installed and installs and loads
+#' all requested packages. Additionally, the package warnings are suppressed.
+#' @param myPackages List all packages needed. There is no default.
 #' @keywords library, install.packages
 #' @export
 #' @examples
-#' my.packages <- c('dplyr','ggplot2')
-#' myLibrary(my.packages)
+#' myPackages <- c('dplyr','ggplot2')
+#' myLibrary(myPackages)
 
-myLibrary <- function(my.packages){
+myLibrary <- function(myPackages, suppressPackage = T){
   usePackage <- function(p)
-  {
-    if (!is.element(p, installed.packages()[,1]))
-      install.packages(p, dep = TRUE)
-    require(p, character.only = TRUE)
-  }
+    usePackage <- function(p)
+    {
+      if (!is.element(p, installed.packages()[,1]))
+        install.packages(p, dep = TRUE)
+      if(suppressPackage == T){
+        lapply(p, function(x) suppressMessages(require(x, character.only = TRUE,quietly=TRUE,warn.conflicts = FALSE)))
+      } else {
+        require(p, character.only = TRUE)
+      }
 
-  lapply(my.packages, usePackage)
+  lapply(myPackages, usePackage)
+    }
 }
+
 
