c9f5d40261374f9a43a315216ce79d518cafa3ef
Robert Bischoff <bischrob@gmail.com>
2018-11-06 16:35:08 -0700

    Changed function to accept package names without vectorizing them.

--- R/myLibrary.R
+++ R/myLibrary.R
@@ -1,15 +1,15 @@
 #' A function to install packages if they do not exist
 #'
 #' This function checks if needed packages are installed and installs and loads
-#' all requested packages. Additionally, the package warnings are suppressed.
-#' @param myPackages List all packages needed. There is no default.
+#' all requested packages. 
+#' @param List all packages needed. There is no default.
 #' @keywords library, install.packages
 #' @export
 #' @examples
-#' myPackages <- c('dplyr','ggplot2')
-#' myLibrary(myPackages)
+#' myLibrary('dplyr','ggplot2')
 
-myLibrary <- function(myPackages){
+myLibrary <- function(...){
+  myPackages <- c(...)
   usePackage <- function(p) 
   {
     if (!is.element(p, installed.packages()[,1]))
